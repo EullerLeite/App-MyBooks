9ed139e37d72fe8889cfdde3623ea6cc40b83a74
EullerLeite <pgeuller3256@gmail.com>
2025-08-06 13:56:02 -0300

    Implementação RecyclerView

--- app/src/main/java/com/euller/mybooks/ui/HomeFragment.kt
+++ app/src/main/java/com/euller/mybooks/ui/HomeFragment.kt
@@ -8,7 +8,9 @@ import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.euller.mybooks.databinding.FragmentHomeBinding
+import com.euller.mybooks.ui.adapter.BookAdapter
 import com.euller.mybooks.viewmodels.HomeViewModel
 
 class HomeFragment : Fragment() {
@@ -17,15 +19,23 @@ class HomeFragment : Fragment() {
     private val binding get() = _binding!!
 
     private val viewModel: HomeViewModel by viewModels()
+    private val adapter: BookAdapter = BookAdapter()
+
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-
-
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
+
+        binding.recyclerviewBooks.layoutManager = LinearLayoutManager(context)
+
+        binding.recyclerviewBooks.adapter = adapter
+
+        viewModel.getAllBooks()
+
+        setObserves()
 
         return binding.root
     }
@@ -33,5 +43,11 @@ class HomeFragment : Fragment() {
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    private fun setObserves() {
+        viewModel.books.observe(viewLifecycleOwner) {
+            adapter.updateBooks(it)
+        }
     }
 }
