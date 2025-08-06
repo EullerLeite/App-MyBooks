8684fae95fc0a780d0ff031478e6a866ff12ffbe
EullerLeite <pgeuller3256@gmail.com>
2025-08-06 11:33:46 -0300

    Vetores e packs

--- app/src/main/java/com/euller/mybooks/viewmodels/FavoriteViewModel.kt
+++ app/src/main/java/com/euller/mybooks/viewmodels/FavoriteViewModel.kt
@@ -1,13 +1,13 @@
-package com.euller.mybooks.ui.notifications
+package com.euller.mybooks.viewmodels
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class NotificationsViewModel : ViewModel() {
+class FavoriteViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is notifications Fragment"
+        value = "This is dashboard Fragment"
     }
     val text: LiveData<String> = _text
 }
